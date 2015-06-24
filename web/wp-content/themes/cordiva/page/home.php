8be9bbdb38c7cfc2b2ea9c96509e657aa54b3270
Gilles Dao Duc <gillesdaoduc@iMac-de-Allan.local>
2015-06-24 18:27:50 +0200

    footer + begin latest news

--- web/wp-content/themes/cordiva/page/home.php
+++ web/wp-content/themes/cordiva/page/home.php
@@ -3,10 +3,19 @@
 Template Name: home template
  */
 
-// require_once( get_template_directory() . '/includes/classes/LivingForceItem.php' );
-// $living_force = new LivingForceItem();
+require_once( get_template_directory() . '/includes/classes/LatestNewsItem.php' );
+$latest_news = new LatestNewsItem();
 
 $context = Timber::get_context();
+
+$post_per_page = 6;
+$category = "latest_news";
+
+$args = $latest_news->getArgs($post_per_page, $category);
+$result = $latest_news->getLatestNews($args);
+
+$context['latest_news'] = $result;
+
 
 $templates = array( 'page-templates/home.twig' );
 
