d7db503ee96f71e2293f1e0a1fd93f941b38b2ea
Gilles Dao Duc <daogilles@pc-14302.home>
2015-07-01 23:38:04 +0200

    page content

---
+++ web/wp-content/themes/cordiva/includes/classes/PageContentItem.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gillesdaoduc
+ * Date: 30/06/2015
+ */
+
+class PageContentItem {
+
+    public function __construct(){
+
+    }
+
+    public function getArgs(){
+
+        $args = array(
+            'post_type' => 'page',
+            'post_status'=>'publish',
+            'post_parent' => '0',
+            'post__in' => array( 11, 13, 15, 17, 19 ),
+            'orderby' => 'date',
+            'order'   => 'DESC',
+        );
+
+        return $args;
+    }
+
+    public function getPageContentItem( $args ){
+
+        $query = new WP_Query( $args );
+        $tab_result = array();
+        if ( $query->have_posts() ) {
+            while ($query->have_posts()) {
+                $query->the_post();
+                $tab_result["author"] = get_the_author();
+                $tab_result["link"] = get_permalink();                
+                $tab_result["title"] = $query->post->post_title;                
+                $originalDate = $query->post->post_date;
+                $newDate = date("d M Y", strtotime($originalDate));
+                $tab_result["date"] = $newDate;
+                
+                $image = get_field('image');
+                $tab_result["image"] = wpthumb($image['url'],'height=500&crop=1');
+                $tab_result["titre"] = get_field('titre');
+                $tab_result["sous_titres"] = get_field('sous_titres');
+
+                $pages = get_pages('child_of='.$query->post->ID.'&sort_column=post_date&meta_key=image_child&sort_order=desc&parent='.$query->post->ID); 
+                $count = 0; 
+                foreach($pages as $page) { 
+                    $content = $page->post_content; 
+                    if(!$content) continue;                                     
+                    $images = get_post_custom($page->ID);
+                    $imageID = $images['image_child'][0];
+                    $imageUrl = wp_get_attachment_image_src($imageID);                    
+                    // $tab_result['baba'][$count]['image_child'] = $imageUrl[0];
+                    $page->image_child = $imageUrl[0];
+                    $tab_result["subpage"][$count] = $page;
+
+                    
+                    $count++;                    
+                }
+                
+            }
+        }
+        wp_reset_query();
+        return $tab_result;
+    }
+
+}
---
+++ web/wp-content/themes/cordiva/page/page-content.php
@@ -0,0 +1,18 @@
+<?php
+/**
+* Template Name: Content template
+*/
+
+require_once( get_template_directory() . '/includes/classes/PageContentItem.php' );
+$pageContent = new PageContentItem();
+
+$context = Timber::get_context();
+
+$args = $pageContent->getArgs();
+$result = $pageContent->getPageContentItem($args);
+
+$context["page"] = $result;
+
+$templates = array( 'page-templates/page-content.twig' );
+
+Timber::render($templates, $context);
